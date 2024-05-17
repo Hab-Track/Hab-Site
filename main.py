b31c927fcb01fc01bb1a726cca2295e173e5eee1
Laande <laande.pro@gmail.com>
2024-05-17 16:46:48 +0200

    k

--- main.py
+++ main.py
@@ -6,22 +6,16 @@ from starlette.routing import Mount
 from website.flask_app import app as flask_app
 from website.api_app import app as fastapi_app
 
+# Envelopper l'application Flask avec WSGIMiddleware
+flask_asgi_app = StarletteWSGIMiddleware(flask_app)
 
-def create_app():
-    # Envelopper l'application Flask avec WSGIMiddleware
-    flask_asgi_app = StarletteWSGIMiddleware(flask_app)
+# Créer une application Starlette pour monter les applications
+routes = [
+    Mount('/api', app=fastapi_app),
+    Mount('/', app=flask_asgi_app),
+]
 
-    # Créer une application Starlette pour monter les applications
-    routes = [
-        Mount('/api', app=fastapi_app),
-        Mount('/', app=flask_asgi_app),
-    ]
-
-    return Starlette(routes=routes)
-
-
-app = create_app()
-
+app = Starlette(routes=routes)
 
 if __name__ == "__main__":
     uvicorn.run("main:app")
