67a027755748c97ca511bec83dca4d0a3644104a
Lande <laande.pro@gmail.com>
2025-02-02 20:03:36 +0100

    better func to create plots

--- utils/plot_functions.py
+++ utils/plot_functions.py
@@ -4,41 +4,38 @@ def create_plot_for_category(data, category, show_active_only):
     fig = go.Figure()
     retros_data = {}
     last_date = max(data.keys())
-    
+
     for date, date_data in data.items():
+        date_data = data[date]
         for retro, stats in date_data.items():
             if retro not in retros_data:
-                retros_data[retro] = {'dates': [], 'heights': []}
-            heights = []
-            for stat in stats:
-                if category in stat:
-                    heights.append(int(stat.split()[0]))
+                retros_data[retro] = {'dates': [], 'values': []}
+            
+            value = next((int(s.split()[0]) for s in stats if s.endswith(f" {category}")), None)
             retros_data[retro]['dates'].append(date)
-            retros_data[retro]['heights'].append(heights[0] if heights else None)
-    
-    sorted_retros = sorted(retros_data.keys())
-    
-    for retro in sorted_retros:
-        retro_data = retros_data[retro]
-        
-        if show_active_only and last_date not in retro_data['dates']:
+            retros_data[retro]['values'].append(value)
+
+    for retro in retros_data.keys():
+        rd = retros_data[retro]
+        if show_active_only and last_date not in rd['dates']:
             continue
-        
+
         fig.add_trace(go.Scatter(
-            x=retro_data['dates'], 
-            y=retro_data['heights'], 
-            mode='lines+markers', 
-            name=retro, 
-            marker=dict(size=10)
+            x=rd['dates'],
+            y=rd['values'],
+            mode='lines+markers',
+            name=retro,
+            marker=dict(size=8),
+            line=dict(width=2)
         ))
-    
+
     fig.update_layout(
-        title=f'{category.capitalize()}', 
-        xaxis_title='Date', 
-        yaxis_title='Total',
+        title=f'{category.capitalize()}',
+        xaxis_title='Date',
+        yaxis_title='Count',
+        font=dict(color='white'),
         plot_bgcolor='rgba(50,50,50,50)',
         paper_bgcolor='rgba(50,50,50,50)',
-        font=dict(color='white')
     )
     
     return fig.to_html(full_html=False, include_plotlyjs=False)
