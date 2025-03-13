599522ae570c9bd19f35dcafa56c666bc7887d68
Lande <laande.pro@gmail.com>
2025-03-13 23:10:52 +0100

    switching to supabase as db

--- website/utils/search.py
+++ website/utils/search.py
@@ -1,71 +1,71 @@
-import sqlite3
 import time
+import os
 from flask import jsonify
+from supabase import create_client, Client
 
 
 DB_PATH = "retro_data.db"
 ALLOWED_ASSET_TYPES = ['badges', 'furnis', 'clothes', 'effects']
 
-
-def get_db_connection():
-    conn = sqlite3.connect(DB_PATH)
-    conn.row_factory = sqlite3.Row
-    return conn
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 
 def get_retros():
-    with get_db_connection() as conn:
-        retros = conn.execute('SELECT DISTINCT retro FROM badges UNION SELECT DISTINCT retro FROM furnis UNION SELECT DISTINCT retro FROM clothes UNION SELECT DISTINCT retro FROM effects').fetchall()
+    retros = set()
+    tables = ['badges', 'furnis', 'clothes', 'effects']
     
-    return [retro[0] for retro in retros]
+    for table in tables:
+        response = supabase.table(table).select("retro").distinct().execute()
+        for row in response.data:
+            retros.add(row['retro'])
+    
+    return list(retros)
 
 
-def get_retro_urls(conn, selected_categories, selected_retros):
-    url_query = "SELECT retro, type, url FROM retro_urls WHERE retro IN ({}) AND type IN ({})".format(
-        ','.join('?' * len(selected_retros)),
-        ','.join('?' * len(selected_categories))
-    )
-    url_params = selected_retros + selected_categories
-    return {(row['retro'], row['type']): row['url'] 
-            for row in conn.execute(url_query, url_params).fetchall()}
+def get_retro_urls(selected_categories, selected_retros):
+    supabase_query = supabase.table("retro_urls").select("retro, type, url").in_("retro", selected_retros).in_("type", selected_categories).execute()
+    return {(row['retro'], row['type']): row['url'] for row in supabase_query.data}
 
 
 def search_database(selected_categories, selected_retros, search_query):
-    with get_db_connection() as conn:
-        urls = get_retro_urls(conn, selected_categories, selected_retros)
-        search_results = {}
+    urls = get_retro_urls(selected_categories, selected_retros)
+    search_results = {}
+    
+    for category in selected_categories:
+        if category not in ALLOWED_ASSET_TYPES:
+            raise ValueError("Invalid asset type provided.")
         
-        for category in selected_categories:
-            if category not in ALLOWED_ASSET_TYPES:
-                raise ValueError("Invalid asset type provided.")
+        # query = f"""
+        #     SELECT retro, name 
+        #     FROM {category} 
+        #     WHERE retro IN ({','.join('?' * len(selected_retros))})
+        #     AND name LIKE ?
+        # """
+        # params = selected_retros + [f'%{search_query}%']
+        # rows = conn.execute(query, params).fetchall()
+        response = supabase.table(selected_categories).select("name").eq('retro', retro).like("name", f"%{search_query}%").execute()
+        rows = response.data
+        
+        for row in rows:
+            retro = row['retro']
+            name = row['name']
+            base_url = urls.get((retro, category))
             
-            query = f"""
-                SELECT retro, name 
-                FROM {category} 
-                WHERE retro IN ({','.join('?' * len(selected_retros))})
-                AND name LIKE ?
-            """
-            params = selected_retros + [f'%{search_query}%']
-            rows = conn.execute(query, params).fetchall()
-            
-            for row in rows:
-                retro = row['retro']
-                name = row['name']
-                base_url = urls.get((retro, category))
+            if base_url:
+                extension = ".gif" if category == "badges" else ".nitro"
+                search_results.setdefault(retro, {})
+                category_name = {
+                    'badges': 'Badges',
+                    'furnis': 'Furniture',
+                    'clothes': 'Clothes',
+                    'effects': 'Effects'
+                }[category]
                 
-                if base_url:
-                    extension = ".gif" if category == "badges" else ".nitro"
-                    search_results.setdefault(retro, {})
-                    category_name = {
-                        'badges': 'Badges',
-                        'furnis': 'Furniture',
-                        'clothes': 'Clothes',
-                        'effects': 'Effects'
-                    }[category]
-                    
-                    search_results[retro].setdefault(category_name, {})
-                    index = len(search_results[retro][category_name])
-                    search_results[retro][category_name][str(index)] = (name, f"{base_url}{name}{extension}")
+                search_results[retro].setdefault(category_name, {})
+                index = len(search_results[retro][category_name])
+                search_results[retro][category_name][str(index)] = (name, f"{base_url}{name}{extension}")
     
     return search_results
 
