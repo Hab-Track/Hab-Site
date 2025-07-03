4455976e71b20aa1a669a91786a2797f7c2c52b6
Lande <laande.pro@gmail.com>
2025-07-03 19:06:38 +0200

    remove identical stats

--- website/utils/plot_functions.py
+++ website/utils/plot_functions.py
@@ -28,7 +28,8 @@ def create_plot_for_category(data, category, show_active_only):
             mode='lines+markers',
             name=retro,
             marker=dict(size=8),
-            line=dict(width=2)
+            line=dict(width=2),
+            connectgaps=True
         ))
 
     fig.update_layout(
